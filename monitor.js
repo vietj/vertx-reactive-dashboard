48307e9537ded86e7e2fe3d346bcf998006463e4
Julien Viet <julien@julienviet.com>
2015-04-02 14:43:57 +0200

    Added fatjar

--- monitor.js
+++ monitor.js
@@ -1,10 +1,6 @@
-var metricsService = require("vertx-dropwizard-js/metrics_service");
 vertx.deployVerticle("maven:io.vertx:processmon:1.0-SNAPSHOT::io.vertx.processmon");
 
-var service = metricsService.create(vertx);
-
 vertx.eventBus().localConsumer("processmon", function(msg) {
-  var snapshot = service.getMetricsSnapshot(vertx);
   var process = msg.body();
   var metrics = {};
   metrics[process.pid] = {

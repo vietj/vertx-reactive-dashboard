67c9c02d9bfd707ab4dac59a5b2fe71e3eb1747e
Julien Viet <julien@julienviet.com>
2015-05-07 23:52:29 +0200

    Updates

---
+++ monitor.rb
@@ -0,0 +1,12 @@
+$vertx.deploy_verticle 'maven:io.vertx:processmon:1.0-SNAPSHOT::io.vertx.processmon'
+
+$vertx.event_bus.local_consumer 'processmon' do |msg|
+  process = msg.body
+  metrics = {
+      process['pid'] => {
+          :CPU => process['cpu'],
+          :Mem => process['mem'],
+      }
+  }
+  $vertx.event_bus.publish 'metrics', metrics
+end
